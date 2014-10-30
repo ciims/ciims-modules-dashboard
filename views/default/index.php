1bda0c78e13134642860e77ecde07215f3f921a3
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2014-10-30 15:18:02 -0500

    Dashboard cards FPO

--- views/default/index.php
+++ views/default/index.php
@@ -1,11 +1,14 @@
 <div class="dashboard-cards">
-	<div style="background: #fff; width: 230px; height: 230px; ">foo </div>
-	<div style="background: #fff; width: 230px; height: 230px; ">bar</div>
-	<div style="background: #fff; width: 230px; height: 230px; ;">;et</div>
-	<div style="background: #fff; width: 230px; height: 230px; ">fds</div>
-	<div style="background: #fff; width: 230px; height: 230px; ">asdf</div>
-	<div style="background: #fff; width: 230px; height: 230px; ">aaaaaa</div>
-	<div style="background: #fff; width: 230px; height: 230px; ">fgggg</div>
+	<div style="background: #fff; width: 230px; height: 230px; "></div>
+	<div style="background: #fff; width: 230px; height: 230px; "></div>
+	<div data-ss-colspan="2" style="background: #fff; width: 482px; height: 482px; ;"></div>
+	<div style="background: #fff; width: 230px; height: 230px; "></div>
+	<div style="background: #fff; width: 230px; height: 230px; "></div>
+	<div style="background: #fff; width: 230px; height: 482px; "></div>
+	<div data-ss-colspan="2" style="background: #fff; width: 482px; height: 230px; "></div>
+	<div style="background: #fff; width: 230px; height: 230px; "></div>
+	<div style="background: #fff; width: 230px; height: 230px; "></div>
+	<div style="background: #fff; width: 230px; height: 230px; "></div>
 </div>
 
 <div class="shader"></div>
